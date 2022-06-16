b7b499c228e52abd308a19220f4c1e94133239bd
Manases Lovera <manaseslovera07@gmail.com>
2022-06-16 00:40:51 -0400

    Menu was completed

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-console.log("hola pendejo kun")
+const {showMenu,pause} = require('./helpers/inquirer.js')
+
+function searchCity(){
+    console.log("Buscar ciudad")
+}
+function showHistory(){
+    console.log("Mostrar historial")
+}
+function sendError(){
+    console.log("Numero no valido")
+}
+const menu = async()=>{
+
+    let opt = 0
+
+    do{
+        opt = await showMenu()
+        if(opt == 1){
+            searchCity()
+        }
+        else if(opt == 2){
+            showHistory()
+        }
+        else if(opt != 3){
+            sendError()
+        }
+        if(opt !== 3) await pause()
+        
+    }while(opt !== 3)
+
+}
+
+menu()
--- helpers/inquirer.js
+++ helpers/inquirer.js
@@ -5,11 +5,51 @@ const questions = [
     {
         type:'list',
         name:'option',
+        message:'Que desea hacer?',
         choices:[
             {
                 value:1,
-                name:'1. Crear tarea'
+                name:`${'1.'.yellow} Buscar ciudad`
+            },
+            {
+                value:2,
+                name:`${'2.'.yellow} Historial`
+            },
+            {
+                value:3, 
+                name:`${'3.'.yellow} Salir`
             }
         ]
     }
 ]
+
+const showMenu = async () => {
+    console.clear()
+    
+    console.log('========================='.green)
+    console.log('  Seleccione una opcion'.white)
+    console.log('=========================\n'.green)
+
+    const {option} = await inquirer.prompt(questions)
+
+    return option
+}
+
+const pause = async() => {
+
+    console.log('\n')
+    await inquirer.prompt([
+        {
+            type:'input',
+            name:'nothing',
+            message:`Presiona ${'enter'.green} para continuar`
+        }
+    ])
+}
+
+
+
+module.exports = {
+    showMenu,
+    pause
+}
